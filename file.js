5e42ca607798ec8bacb1d19b3d7f0aa55ecf167c
perehelion <31827598+perehelion@users.noreply.github.com>
2018-01-18 19:00:57 +0200

    trying to solve by XMLHttpRequest

--- file.js
+++ file.js
@@ -4,71 +4,66 @@ let a = 0;
 let b = 0;
 let resultArray = [];
 let stack = [];
+let i = 0;
 
 
 function postfixCalc(expression) {
     stack = [];
     expression = expression.split(' ');
-    for(let i = 0; i < expression.length; i++){
-        switch(expression[i]) {
+    for (i = 0; i < expression.length; i++) {
+        switch (expression[i]) {
             case '+': // a-b
                 b = stack.pop();
                 a = stack.pop();
-                stack.push(a-b);
+                stack.push(a - b);
                 break;
             case '-': // a+b+8 
                 b = stack.pop();
                 a = stack.pop();
-                stack.push(a+b+8);
-            break;
+                stack.push(a + b + 8);
+                break;
             case '*': // a%b
                 b = stack.pop();
                 a = stack.pop();
-                if(b == 0){stack.push(42);}
-                else{stack.push(a%b);};
-            break;
+                if (b == 0) { stack.push(42); }
+                else { stack.push(a % b); };
+                break;
             case '/': // a/b
                 b = stack.pop();
                 a = stack.pop();
-                if(b == 0){stack.push(42)}
-                else{stack.push(a/b);};
-            break;
+                if (b == 0) { stack.push(42) }
+                else { stack.push(a / b); };
+                break;
             default:
                 stack.push(parseInt(expression[i]));
         }
-        // console.log(expression);
-        // console.log(stack, a, b);
     };
     return stack[0];
 };
 
-
+// taking task
 var req = new XMLHttpRequest();
 req.open("GET", "https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task", false);
 req.send(null);
 let task = JSON.parse(req.responseText);
 
+// running alg for all expressions
 for (let index = 0; index < task['expressions'].length; index++) {
     resultArray.push(postfixCalc(task['expressions'][index]));
 };
-// console.log('arr just after calcing',resultArray);
 
+//send result and check result
 var res = new XMLHttpRequest();
 res.open("POST", "https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task", false);
 let sendingMessage = JSON.stringify({
-                        id: task['id'],
-                        results: resultArray,
-                    });
-// sendingMessage = '{ "id":"' + task['id'] + ',"results":"'+ resultArray.toString() +'"}' // \'
-console.log(JSON.parse(sendingMessage)["results"].toString() === resultArray.toString());
+    id: task['id'],
+    results: resultArray,
+});
 
-
-res.setRequestHeader('Content-Type', 'json');
 res.send(sendingMessage);
-
-console.log('res status: ', res.status, res.statusText);
-// console.log(typeof JSON.parse(res.responseText));
+console.log(' \n res status: ', res.status, res.statusText);
 console.log('res.text: ', JSON.parse(res.responseText));
+
 // https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task
 // https://www.eliftech.com/school-task
 
