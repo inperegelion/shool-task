a0303e007819a50bad041310de244cc7f4f4c0c0
perehelion <31827598+perehelion@users.noreply.github.com>
2018-01-18 21:02:27 +0200

    pre-final

--- file2.js
+++ file2.js
@@ -3,10 +3,10 @@ let fetch = require("node-fetch")
 
 let a = 0;
 let b = 0;
-let resultArray = [];
-let id = '';
-let stack = [];
 let i = 0;
+let id = '';
+let resultArray = [];
+let stack = [];
 
 
 function postfixCalc(expression) {
@@ -17,24 +17,24 @@ function postfixCalc(expression) {
             case '+': // a-b
                 b = stack.pop();
                 a = stack.pop();
-                stack.push(a - b);
+                stack.push(parseInt(a - b));
                 break;
             case '-': // a+b+8 
                 b = stack.pop();
                 a = stack.pop();
-                stack.push(a + b + 8);
+                stack.push(parseInt(a + b + 8));
                 break;
             case '*': // a%b
                 b = stack.pop();
                 a = stack.pop();
                 if (b == 0) { stack.push(42); }
-                else { stack.push(a % b); };
+                else { stack.push(parseInt(a % b)); };
                 break;
             case '/': // a/b
                 b = stack.pop();
                 a = stack.pop();
                 if (b == 0) { stack.push(42) }
-                else { stack.push(a / b); };
+                else { stack.push(parseInt(a / b)); };
                 break;
             default:
                 stack.push(parseInt(expression[i]));
@@ -44,23 +44,25 @@ function postfixCalc(expression) {
 };
 
 
-
-
-fetch('https://www.eliftech.com/school-task')
+fetch('https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task')
     .then(function (response) {
         return response.json();
     })
     .then(function (task) {
+/*recieved expr*/ console.log(task['expressions']);
         for (let index = 0; index < task['expressions'].length; index++) {
             resultArray.push(postfixCalc(task['expressions'][index]));
         };
         id = task['id']
+/*sending body*/  console.log(JSON.stringify({ id: task['id'], results: resultArray }));
         return JSON.stringify({ id: task['id'], results: resultArray })
     })
     .then(function (calc) {
-        fetch('https://www.eliftech.com/school-task', { method: 'POST', body: calc })
-            .then(function (res) {
-                console.log(res);
+        fetch('https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task', { method: 'POST', body: calc })
+            .then(response => response.json())
+            .then(function (x) {
+                if (x.passed == true) {
+                    console.log('Hold on, ElifTech School, I\'m coming ')
+                } else { console.log('Hold on, ElifTech School, I almost there') }
             })
     })
-
