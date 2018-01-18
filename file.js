49473fef5836420ffe48400709b68ef942262517
perehelion <31827598+perehelion@users.noreply.github.com>
2018-01-18 14:07:03 +0200

    few corrections left

---
+++ file.js
@@ -0,0 +1,73 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+let a = 0;
+let b = 0;
+let resultArray = [];
+let stack = [];
+
+
+function postfixCalc(expression) {
+    stack = [];
+    expression = expression.split(' ');
+    for(let i = 0; i < expression.length; i++){
+        switch(expression[i]) {
+            case '+': // a-b
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a-b);
+                break;
+            case '-': // a+b+8 
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a+b+8);
+            break;
+            case '*': // a%b
+                b = stack.pop();
+                a = stack.pop();
+                if(b == 0){stack.push(42);}
+                else{stack.push(a%b);};
+            break;
+            case '/': // a/b
+                b = stack.pop();
+                a = stack.pop();
+                if(b == 0){stack.push(42)}
+                else{stack.push(a/b);};
+            break;
+            default:
+                stack.push(parseInt(expression[i]));
+        }
+        // console.log(expression);
+        // console.log(stack, a, b);
+    };
+    return stack[0];
+};
+
+
+var req = new XMLHttpRequest();
+req.open("GET", "https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task", false);
+req.send(null);
+let task = JSON.parse(req.responseText);
+
+for (let index = 0; index < task['expressions'].length; index++) {
+    resultArray.push(postfixCalc(task['expressions'][index]));
+};
+// console.log('arr just after calcing',resultArray);
+
+var res = new XMLHttpRequest();
+res.open("POST", "https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task", false);
+let sendingMessage = JSON.stringify({
+                        id: task['id'],
+                        results: resultArray,
+                    });
+
+console.log(JSON.parse([sendingMessage]).results.toString == resultArray);
+
+res.setRequestHeader('Content-Type', 'json');
+res.send(sendingMessage);
+
+console.log('res status: ', res.status, res.statusText);
+console.log('res.text: ', res.responseText);
+
+// https://u0byf5fk31.execute-api.eu-west-1.amazonaws.com/etschool/task
+// https://www.eliftech.com/school-task
+
